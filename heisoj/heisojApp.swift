4d5ed3eb81a4221c311abb16e7dcd6eb9a604de1
Poyraz Bahcivan <poyrazbahcivan@gmail.com>
2026-09-12 06:31:33 -0400

    Initial Commit

---
+++ heisoj/heisojApp.swift
@@ -0,0 +1,17 @@
+//
+//  heisojApp.swift
+//  heisoj
+//
+//  Created by Poyraz Bahcivan on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct heisojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
